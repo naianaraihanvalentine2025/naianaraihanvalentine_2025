54a4260b0136ccdb3b3b85d71105727fbec40598
naianaraihanvalentine2025 <babybellzeebub@gmail.com>
2025-02-15 17:20:16 +0700

    Add files via upload

---
+++ naia.py
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website Saya</title>
+</head>
+<body>
+    <h1>Selamat datang di Website Saya!</h1>
+    <p>Ini adalah halaman utama dari GitHub Pages.</p>
+</body>
+</html>
+
+import turtle as t
+import random
+
+def makeHeart(size):
+    t.forward(size)
+    t.circle(size/2, 180)
+    t.right(90)
+    t.circle(size/2, 180)
+    t.forward(size)
+    t.left(90)
+
+def ILoveMyGirlfriend(size, length, depth):
+
+    if depth == 0:
+        return
+    ILoveMyGirlfriend(size*1.5, length, depth-1)
+    counter = 360
+    while counter > 0:
+        heartColor = random.randint(100, 255), random.randint(100, 255), random.randint(100, 255)
+        t.color(heartColor)
+        t.fillcolor(heartColor)
+        t.begin_fill()
+        makeHeart(size)
+        t.end_fill()
+        t.left(45)
+        counter = counter -45
+
+def main():
+    t.speed(0)
+    t.colormode(255)
+
+    ILoveMyGirlfriend(20,0,8)
+    t.back(100)
+    t.color("black")
+    t.write("I love you", move=False, align="left", font=("Courier New", 24, "bold"))
+    t.done()
+
+main()
